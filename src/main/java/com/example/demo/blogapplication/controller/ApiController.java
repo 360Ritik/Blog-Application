4802cdc3b89fbff13de20b7121d705009715130d
360Ritik <ritikkp111@gmail.com>
2023-05-08 14:02:42 +0530

    project Initialized

---
+++ src/main/java/com/example/demo/blogapplication/controller/ApiController.java
@@ -0,0 +1,11 @@
+package com.example.demo.blogapplication.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ApiController {
+
+
+    
+}
